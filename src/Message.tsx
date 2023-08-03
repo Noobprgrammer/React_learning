f1d08cf6ec4f983418ccb098b8fd225d4c3112b9
GoatKy1e <meekail.mugdho@gmail.com>
2023-08-03 16:25:57 +0600

    update: the use of constants in the message tsx

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,11 +1,14 @@
 function Message(){
-    return <h1 className = "pepepopo " >Hello World</h1>;
+    const name = 'Meekail';
+    return (
+        <h1 className = "pepepopo " >
+            Hello {name}
+        </h1>
+    );
 }
 
 export default Message;
 /*  
-    >Created new tsx folder under the name Message,
-    > Created a function "Message"
-    > The Message function returns a HTML <h1> element
-    > exporting the whole Message function to be used in a different Typescript file. (refer to App.tsx)
+    >created a constant variable which contains a name 
+    >the variable is being used in the <h1> element
 */
